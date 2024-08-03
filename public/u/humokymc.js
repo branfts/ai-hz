1e5a07cd6bb18ea1dbc4d33823b80991c8af5b9f
branfts <support@june07.com>
2024-08-02 21:24:01 -0700

    Updated by A10x Service

---
+++ public/u/humokymc.js
@@ -0,0 +1,13 @@
+
+const links = [
+    { url: 'https://youtube.com/@HumokyMC' },
+{ url: 'https://tiktok.com/@humoky' },
+{ url: 'https://instagram.com/humokymc_' },
+{ url: 'https://discord.gg/bBj3xkkfpX' },
+{ url: 'https://linktr.ee/humokymc' }
+]
+const rules = [
+    
+]
+
+export { links, rules }
